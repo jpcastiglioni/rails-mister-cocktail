c1d02f356ed26605497338abcf3067e4eacbb569
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-06-20 11:51:37 -0300

    Added json seed for ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Erradicating database for good"
+
+Ingredient.destroy_all
+
+puts "Seeding ingredients from the cocktailbd"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_json = open(url).read
+ingredient_hash = JSON.parse(ingredient_json)
+ingredient_array = ingredient_hash['drinks']
+
+ingredient_array.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+# puts "#{user['name']} - #{user['bio']}"
